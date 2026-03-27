2254a8a61931484973e749361da751815a47a2c5
Chernikov Nikita <14nik20@gmail.com>
2026-03-27 05:17:24 +0200

    Changed chroma collection name

--- db_setup.py
+++ db_setup.py
@@ -4,7 +4,7 @@ from langchain_chroma import Chroma
 def get_db():
     embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
     vector_store = Chroma(
-        collection_name="aws_docs",
+        collection_name="research_assistant_data",
         embedding_function=embeddings,
         persist_directory="./chroma",
         collection_metadata={
